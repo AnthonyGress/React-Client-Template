08924065821982f754a9355f21c5e2b701bbea30
Anthony Gress <the.techx.guy@gmail.com>
2021-09-17 13:20:09 -0400

    scroll to top fix

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -7,27 +7,30 @@ import {
   Redirect,
 } from "react-router-dom";
 import { About, Home, Contact, Gallery } from "../pages";
+import { ScrollToTop } from "./ScrollTopTop";
 
 const Routes = () => {
   return (
     <Router basename={process.env.PUBLIC_URL}>
-      <main className="App">
-        <Switch>
-          <Route exact={true} path={"/"}>
-            <Home />
-          </Route>
-          <Route path={process.env.PUBLIC_URL + "/about"}>
-            <About />
-          </Route>
-          <Route path={process.env.PUBLIC_URL + "/gallery"}>
-            <Gallery />
-          </Route>
-          <Route path={process.env.PUBLIC_URL + "/contact"}>
-            <Contact />
-          </Route>
-          <Redirect from="*" to="/" />
-        </Switch>
-      </main>
+      <ScrollToTop>
+        <main className="App">
+          <Switch>
+            <Route exact={true} path={"/"}>
+              <Home />
+            </Route>
+            <Route path={process.env.PUBLIC_URL + "/about"}>
+              <About />
+            </Route>
+            <Route path={process.env.PUBLIC_URL + "/gallery"}>
+              <Gallery />
+            </Route>
+            <Route path={process.env.PUBLIC_URL + "/contact"}>
+              <Contact />
+            </Route>
+            <Redirect from="*" to="/" />
+          </Switch>
+        </main>
+      </ScrollToTop>
     </Router>
   );
 };
